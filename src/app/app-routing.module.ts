8a1002c2f9af6bb5f9112db2e820913028032d5e
k.shushkovsky <k.shushkovskii@ispsystem.com>
2020-05-25 03:36:33 +0800

    feat: реализация ленивой подгрузки переводов

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   pathMatch: 'full',
-  //   redirectTo: 'home'
-  // },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
